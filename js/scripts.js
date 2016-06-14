a445d86e4a854b99ce926f0087dec4662cf8ff82
Joel Delight and Wolfgang Warneke <joel@>
2016-06-14 08:59:09 -0700

    add senior discount

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,22 @@ function Movie(name, releaseDate, rating) {
   this.showTimes = [];
 };
 
+var basePrice = 10;
 var now = new Date();
 var fullDaysSinceEpoch = Math.floor(now/8.64e7);
 
 var actionTwo = new Movie("Action Two: Action Gets Bigger", 16950, "R");
 
-Movie.prototype.price = function() {
-  return "$10";
+Movie.prototype.price = function(userAge) {
+  var ticketPrice;
+  if (userAge >= 60) {
+    ticketPrice = basePrice * 0.8;
+  } else if (userAge <= 12) {
+
+  } else {
+    ticketPrice = basePrice;
+  }
+  return ticketPrice;
 }
 
 
