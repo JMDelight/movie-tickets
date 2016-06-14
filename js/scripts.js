691a25b2b8662669b0e84ccead8d56c63af07a79
Joel Delight and Wolfgang Warneke <joel@>
2016-06-14 11:43:23 -0700

    add shopping cart and behavior for said item

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+var userName;
+var userEmail;
+var userCreditCard;
+var totalCost = 0;
+var tickets = [];
+var ticketType;
+var basePrice = 10;
+var nowMilliseconds = new Date();
+var nowDays = Math.floor(nowMilliseconds/8.64e7);
+
 function Movie(name, releaseDate, rating) {
   this.movieName = name;
   this.releaseDate = releaseDate;
@@ -5,14 +15,6 @@ function Movie(name, releaseDate, rating) {
   this.showTimes = [];
 };
 
-var userName;
-var userEmail;
-var totalCost = 0;
-
-
-var basePrice = 10;
-var nowMilliseconds = new Date();
-var nowDays = Math.floor(nowMilliseconds/8.64e7);
 Movie.prototype.isNewRelease = function() {
   if (this.releaseDate < nowDays - 180) {
     return true;
@@ -21,17 +23,17 @@ Movie.prototype.isNewRelease = function() {
   }
 }
 
-var movieTwo = new Movie("Action Two: Action Gets Bigger", 16950, "R");
-var movieOne = new Movie("Action One: the Birth of Action", 16750, "PG-13");
-
 Movie.prototype.price = function(userAge, showTime) {
   var ticketPrice;
   if (userAge >= 60) {
     ticketPrice = basePrice * 0.8;
+    ticketType = "Senior";
   } else if (userAge <= 12) {
     ticketPrice = basePrice * 0.85;
+    ticketType = "Kid";
   } else {
     ticketPrice = basePrice;
+    ticketType = "Normal";
   }
   if (showTime < 16) {
     ticketPrice *= 0.6;
@@ -42,8 +44,25 @@ Movie.prototype.price = function(userAge, showTime) {
   return ticketPrice;
 }
 
+var movieTwo = new Movie("Action Two: Action Gets Bigger", 16950, "R");
+var movieOne = new Movie("Action One: the Birth of Action", 16750, "PG-13");
+var movieThree = new Movie("Bruce Willis: The True Story", 16770, "PG");
 
 $(document).ready(function() {
+
+  $('#movie-1-form').submit(function(event) {
+    alert("One");
+    event.preventDefault();
+    userName = $('#movie-1-name').val();
+    userEmail = $('#movie-1-email').val();
+    var showTime = $('#movie-1-time option:selected').val();
+    var age = $('input[name=movie-1-age]:checked').val();
+    totalCost += movieOne.price(age, showTime);
+    var ticket = "One " + ticketType + " ticket for " + movieOne.movieName + " at " + showTime;
+    alert("One2")
+    tickets.push(ticket);
+    console.log(totalCost);
+  });
 
   $('#movie-2-form').submit(function(event) {
     event.preventDefault();
@@ -52,7 +71,39 @@ $(document).ready(function() {
     var showTime = $('#movie-2-time option:selected').val();
     var age = $('input[name=movie-2-age]:checked').val();
     totalCost += movieTwo.price(age, showTime);
+    var ticket = "One " + ticketType + " ticket for " + movieTwo.movieName + " at " + showTime;
+    tickets.push(ticket);
     console.log(totalCost);
+  });
+
+  $('#movie-3-form').submit(function(event) {
+    event.preventDefault();
+    userName = $('#movie-3-name').val();
+    userEmail = $('#movie-3-email').val();
+    var showTime = $('#movie-3-time option:selected').val();
+    var age = $('input[name=movie-3-age]:checked').val();
+    totalCost += movieThree.price(age, showTime);
+    var ticket = "One " + ticketType + " ticket for " + movieThree.movieName + " at " + showTime;
+    tickets.push(ticket);
+    console.log(totalCost);
+  });
+
+  $('#shopping-cart-button').click(function() {
+    $('#ticket-list').empty();
+    tickets.forEach(function(ticket) {
+      $('#ticket-list').append('<li>' + ticket + '</li>');
+    });
+  });
+
+  $("#shopping-cart").submit(function(event) {
+    event.preventDefault();
+    userCreditCard = $('#credit-card').val();
+    alert("Your tickets have been purchased successfully.");
+    userName = "";
+    userEmail = "";
+    userCreditCard = "";
+    totalCost = 0;
+    tickets = [];
 
   });
 
