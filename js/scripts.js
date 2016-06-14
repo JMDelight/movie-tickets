babfe789bd5983e5741426c5642f0e31e5f25d07
Joel Delight and Wolfgang Warneke <wolfgang@>
2016-06-14 08:52:37 -0700

    add functionality for an example movie to return a basic price of ten dollars

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
-function movie(name, releaseDate, rating) {
+function Movie(name, releaseDate, rating) {
   this.movieName = name;
   this.releaseDate = releaseDate;
   this.rating = rating;
   this.showTimes = [];
+};
+
+var now = new Date();
+var fullDaysSinceEpoch = Math.floor(now/8.64e7);
+
+var actionTwo = new Movie("Action Two: Action Gets Bigger", 16950, "R");
+
+Movie.prototype.price = function() {
+  return "$10";
 }
 
 
